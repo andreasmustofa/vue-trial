0b5aca9d7efb1a7e3ad047994f6c4a83b6b5b21d
Andreas Mustofa <andreitnet.gny@gmail.com>
2025-07-26 07:40:23 +0700

    first commit

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+
+const store = useCounterStore();
+</script>
+
+<template>
+    <h1>Dashboard</h1>
+    <h2>Halaman Dashboard</h2>
+    <div>Saya Andreas Mustofa, seorang software developer</div>
+
+    <div>
+        <button @click="store.increment">Count is {{ store.count }}</button>
+    </div>
+</template>
+
+<style>
+</style>
